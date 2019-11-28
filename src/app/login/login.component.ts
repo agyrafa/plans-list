390c6e9c9be83686629b1928368539d0f1eae61a
Marianna Almeida <agyrafa@gmail.com>
2019-11-28 17:57:27 -0300

    Minor fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { ApiService } from "../_services/api.service";
-import { NgForm, FormControl } from "@angular/forms";
-import { Router } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../_services/api.service';
+import { NgForm, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   formData = {
-    login: "",
-    password: ""
+    login: '',
+    password: ''
   };
 
   constructor(
@@ -27,7 +27,7 @@ export class LoginComponent implements OnInit {
     if (loginForm.login && loginForm.password) {
       this.apiService.postLogin(loginForm).subscribe(
         next => {
-          this.router.navigate(["/plans"]);
+          this.router.navigate(['/plans']);
         },
         error => {
           alert(error);
